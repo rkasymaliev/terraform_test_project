46b566d702ecaa4d1bb7d7944437ae370eda7b36
rkasymaliev <94782500+rkasymaliev@users.noreply.github.com>
2022-04-04 11:42:43 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "ec2-instance" {
   Your_Last_Name = var.Your_Last_Name
   region = var.region
   access_key = var.access_key
-  asecret_key = var.asecret_key
+  secret_key = var.secret_key
   tls_key_filename = var.tls_key_filename
   key_pair_name = var.key_pair_name
   instance_type = var.instance_type
